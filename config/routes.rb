f84f43298d28ebff7d8c4dd33f3aad01696ee770
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-09 16:33:20 +0100

    style splash page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
+    get 'login', to: 'devise/sessions#new'
     get 'sign_up', to: 'devise/registrations#new'
   end
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,12 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ['Raleway', 'sans-serif']
+      }
+    }
+  },
 }
