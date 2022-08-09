f820407bc1ef8e8678014492624dab0d7ad9dd4d
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-09 10:27:52 +0100

    fix linters

--- spec/views/payments/edit.html.erb_spec.rb
+++ spec/views/payments/edit.html.erb_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "payments/edit", type: :view do
+RSpec.describe 'payments/edit', type: :view do
   before(:each) do
     @payment = assign(:payment, Payment.create!(
-      name: "MyString",
-      amount: "9.99"
-    ))
+                                  name: 'MyString',
+                                  amount: '9.99'
+                                ))
   end
 
-  it "renders the edit payment form" do
+  it 'renders the edit payment form' do
     render
 
-    assert_select "form[action=?][method=?]", payment_path(@payment), "post" do
+    assert_select 'form[action=?][method=?]', payment_path(@payment), 'post' do
+      assert_select 'input[name=?]', 'payment[name]'
 
-      assert_select "input[name=?]", "payment[name]"
-
-      assert_select "input[name=?]", "payment[amount]"
+      assert_select 'input[name=?]', 'payment[amount]'
     end
   end
 end
--- spec/views/payments/index.html.erb_spec.rb
+++ spec/views/payments/index.html.erb_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "payments/index", type: :view do
+RSpec.describe 'payments/index', type: :view do
   before(:each) do
     assign(:payments, [
-      Payment.create!(
-        name: "Name",
-        amount: "9.99"
-      ),
-      Payment.create!(
-        name: "Name",
-        amount: "9.99"
-      )
-    ])
+             Payment.create!(
+               name: 'Name',
+               amount: '9.99'
+             ),
+             Payment.create!(
+               name: 'Name',
+               amount: '9.99'
+             )
+           ])
   end
 
-  it "renders a list of payments" do
+  it 'renders a list of payments' do
     render
-    assert_select "tr>td", text: "Name".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
+    assert_select 'tr>td', text: 'Name'.to_s, count: 2
+    assert_select 'tr>td', text: '9.99'.to_s, count: 2
   end
 end
