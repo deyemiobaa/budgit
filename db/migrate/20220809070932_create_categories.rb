aede8691ee2fe479700f02679150a3ec9ec7a2cf
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-09 10:04:22 +0100

    generate payments scaffold

--- db/migrate/20220809070932_create_categories.rb
+++ db/migrate/20220809070932_create_categories.rb
@@ -2,9 +2,9 @@ class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
       t.string :name
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_reference :categories, :user, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_070932) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_084155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,10 +44,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_070932) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_payments_on_category_id"
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,4 +77,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_070932) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "categories", "users"
+  add_foreign_key "payments", "categories"
+  add_foreign_key "payments", "users"
 end
