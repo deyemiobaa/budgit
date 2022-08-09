6f1dfff8dd8113899053e3ce66635f8da6ce5668
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-09 10:19:05 +0100

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   resources :payments
   resources :categories
   devise_for :users
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
+
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+
+  root to: 'welcome#index'
 end
