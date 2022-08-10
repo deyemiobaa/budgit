cf502026d3992f42262b2f5a84b677bcf398eff7
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-10 14:34:25 +0100

    fix signup authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   has_many :payments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :payments
   resources :categories
-  devise_for :users
-
-  devise_scope :user do
-    get 'login', to: 'devise/sessions#new'
-    get 'sign_up', to: 'devise/registrations#new'
-  end
-
+  
   authenticated :user do
     root 'categories#index', as: :authenticated_root
   end
