1210743bfa4ea3d9fc10892a7a8eeed930c2745f
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-11 14:23:52 +0100

    add validations to payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,10 @@
 class Payment < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  validates :amount, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+  validates :category_id, presence: true
+  validates :name, presence: true
 
   def payment_date
     self.created_at.strftime("%d %b %Y")
